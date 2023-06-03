db7b74762633ff8a7bc42541df73944106f82799
Adam Elaoud <adam.elaoud@gmail.com>
2023-06-03 10:51:54 -0600

    NEW: prep for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+    base: "/",
+    plugins: [react()],
+    resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+        '@': path.resolve(__dirname, './src'),
     },
-  },
+    },
 })
