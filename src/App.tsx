03cdd94d815445c6e6e75af770d0cdccd46a8d10
Adam Elaoud <adam.elaoud@gmail.com>
2023-08-20 14:45:58 -0600

    FIX: leftover prop

--- src/App.tsx
+++ src/App.tsx
@@ -212,7 +212,6 @@ const App = () : JSX.Element => {
                                         style = {dynamicWidth}
                                         onClick = {() => { navigate(PageRoute.QuizSelect); resetWizard(state => !state); }}
                                         openSettings = {() => setShowSettings(true)}
-                                        isInQuiz = {isInQuiz}
                                     />
 
                                     <div key = {`page-${pageKey}`} ref = {pageRef} className = {pageClasses} style = {dynamicWidth}>
